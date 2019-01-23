f8754871b1f4e1b7cacf7297e6b65edc6a1c388b
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2019-01-23 16:45:24 +0000

    Implement Devise

--- film_db/config/routes.rb
+++ film_db/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  # get 'films/index'
+  devise_for :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :films, only: [:index, :edit, :update]
+
+  root to: 'films#index'
 end
---
+++ film_db/db/migrate/20190123163336_change_email_props.rb
@@ -0,0 +1,5 @@
+class ChangeEmailProps < ActiveRecord::Migration[5.2]
+  def change
+    change_column :users, :email, :string, null: false, default: ''
+  end
+end
