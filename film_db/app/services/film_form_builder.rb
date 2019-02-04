03641838ea7c8369b6790fb0d1d41ac63fe282a7
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2019-02-04 17:47:52 +0000

    WIP

--- film_db/app/services/film_form_builder.rb
+++ film_db/app/services/film_form_builder.rb
@@ -5,7 +5,9 @@ class FilmFormBuilder < ActionView::Helpers::FormBuilder
   def text_field(attribute, options = {})
     content_tag(:div, class: 'govuk-form-group') do
       options = {class: 'govuk-!-width-one-half govuk-input', 'aria-describedby'=> ''}
-      label(attribute, class: 'govuk-label') + super(attribute, options)
+      label_html = label(attribute, class: 'govuk-label')
+      text_field_html = super(attribute, options)
+      label_html + text_field_html
     end
   end
 
