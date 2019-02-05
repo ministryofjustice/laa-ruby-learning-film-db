5c2fe17752705717e399dd05d061643f6a39165f
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2019-02-05 09:56:44 +0000

    Add customisation and submit button

--- film_db/app/services/film_form_builder.rb
+++ film_db/app/services/film_form_builder.rb
@@ -4,12 +4,19 @@ class FilmFormBuilder < ActionView::Helpers::FormBuilder
 
   def text_field(attribute, options = {})
     content_tag(:div, class: 'govuk-form-group') do
-      options = {class: 'govuk-!-width-one-half govuk-input', 'aria-describedby'=> ''}
-      label_html = label(attribute, class: 'govuk-label')
-      text_field_html = super(attribute, options)
+      label_text = options.delete(:label)
+      text_field_options = {class: 'govuk-!-width-one-half govuk-input', 'aria-describedby'=> ''}.merge(options)
+      label_html = label(attribute, label_text, class: 'govuk-label')
+      text_field_html = super(attribute, text_field_options)
       label_html + text_field_html
     end
   end
 
+
+  def submit(button_text = nil, options ={})
+    content_tag(:button, class: 'govuk-button') do
+      button_text || "Save"
+    end
+  end
 end
 
